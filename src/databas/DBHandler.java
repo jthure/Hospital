de26b77c5a48230d764ef59f91f6d711b57ec201
Jonas Thuresson <dat13jth@student.lu.se>
2016-02-29 14:42:09 +0100

    write command

--- src/databas/DBHandler.java
+++ src/databas/DBHandler.java
@@ -12,16 +12,17 @@ import java.util.Scanner;
 
 import com.sun.javafx.geom.transform.GeneralTransform3D;
 
+import model.Doctor;
 import model.User;
 
 public class DBHandler {
-	
+
 	HashMap<String, Journal> map;
 	ArrayList<Journal> journalList;
 	FileWriter fw = null;
 	Scanner scan = null;
-	
-	public DBHandler(){
+
+	public DBHandler() {
 		journalList = new ArrayList<>();
 		map = new HashMap<>();
 		try {
@@ -29,11 +30,11 @@ public class DBHandler {
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
+
 		String info = null;
-		String pnr,id,name,division,doctor,nurse,data = "";
+		String pnr, id, name, division, doctor, nurse, data = "";
 		scan.nextLine();
-		while(scan.hasNextLine()){
+		while (scan.hasNextLine()) {
 			info = scan.nextLine();
 			pnr = info.split(";")[0];
 			id = info.split(";")[1];
@@ -42,13 +43,13 @@ public class DBHandler {
 			doctor = info.split(";")[4];
 			nurse = info.split(";")[5];
 			data = info.split(";")[6];
-			
+
 			map.put(id, new Journal(pnr, id, name, division, doctor, nurse, data));
 		}
 		scan.close();
 	}
-	
-	private void updateDB(){
+
+	private void updateDB() {
 		PrintWriter pw = null;
 		try {
 			pw = new PrintWriter("journal.txt");
@@ -56,48 +57,47 @@ public class DBHandler {
 			// TODO: handle exception
 		}
 		pw.println("Pnr;id;name;division;doctor;nurse;data");
-		for(Journal j : map.values()){
+		for (Journal j : map.values()) {
 			pw.println(j.toString());
 		}
 	}
 
-//	public HashMap<String, Journal> getMap(){
-//		return map;
-//	}
-	
-	public ArrayList<Journal> read(String wanted){
-		
-		if(wanted.length() == 10){
+	// public HashMap<String, Journal> getMap(){
+	// return map;
+	// }
+
+	public ArrayList<Journal> read(String wanted) {
+
+		if (wanted.length() == 10) {
 			return readByPnr(wanted);
-		}
-		else{
+		} else {
 			return readById(wanted);
 		}
 	}
-	
-	public ArrayList<Journal> readByPnr(String pNr){
+
+	public ArrayList<Journal> readByPnr(String pNr) {
 		journalList.clear();
-		for(Journal j : map.values()){
-			if (pNr.equals(j.getPnr())){
+		for (Journal j : map.values()) {
+			if (pNr.equals(j.getPnr())) {
 				journalList.add(j);
 			}
 		}
-		
+
 		return journalList;
 	}
-	
-	public ArrayList<Journal> readById(String id){
+
+	public ArrayList<Journal> readById(String id) {
 		Journal journal = map.get(id);
 		journalList.clear();
-		if(journal != null){
+		if (journal != null) {
 			journalList.add(journal);
 			return journalList;
 		}
 		return null;
 	}
-	
-	public boolean add(Journal journal) throws IOException{
-		if (map.containsKey(journal.getId())){
+
+	public boolean add(Journal journal) throws IOException {
+		if (map.containsKey(journal.getId())) {
 			return false;
 		}
 		map.put(journal.getId(), journal);
@@ -110,36 +110,45 @@ public class DBHandler {
 		fw.close();
 		return true;
 	}
-	
-	public boolean write(String id, String data, User user , boolean append){
+
+	public boolean write(String id, String data, User user, boolean append) {
 		Journal j = map.get(id);
-		if (j != null){
-			
-		} else if(user.getType() == "Doctor"){
-			if (j.getDoctor() == user.getName()) {
+		if (j == null) {
+			return false;
+		} else if (user.getType() == Doctor.class.getSimpleName()) {
+			if (j.getDoctor().equals(user.getName())) {
 				j.write(data, append);
+				updateDB();
+				return true;
 			}
-		} 
-		updateDB();
+		} else if (j.getNurse().equals(user.getName())) {
+			j.write(data, append);
+			updateDB();
+			return true;
+
+		}
+
 		return false;
 	}
-	
-	public boolean delete(String id){
-		return map.remove(id)!=null?true:false;
-	}
-	
-//	public static void main(String[] args) throws IOException{
-//		DBHandler db = new DBHandler();
-//		System.out.println(db.read("1").get(0).toString());
-//		Journal j = new Journal("0402213138", "Jonas Thuresson", "barnmotagningen", "Alexander", "Salas", "Jonas har problem med alkoholen");
-//		Journal jb = new Journal("0402213138", "Jonas Thuresson", "barnmotagningen", "Alexander", "Salas", "Jonas måste skickas till rehab");
-//		db.add(j);
-//		db.add(jb);
-//		System.out.println(db.read("0402213138").get(0).toString());
-//		System.out.println(db.read("0402213138").get(1).toString());
-//		
-//	}
 
-			
-	
+	public boolean delete(String id) {
+		return map.remove(id) != null ? true : false;
+	}
+
+	// public static void main(String[] args) throws IOException{
+	// DBHandler db = new DBHandler();
+	// System.out.println(db.read("1").get(0).toString());
+	// Journal j = new Journal("0402213138", "Jonas Thuresson",
+	// "barnmotagningen", "Alexander", "Salas", "Jonas har problem med
+	// alkoholen");
+	// Journal jb = new Journal("0402213138", "Jonas Thuresson",
+	// "barnmotagningen", "Alexander", "Salas", "Jonas måste skickas till
+	// rehab");
+	// db.add(j);
+	// db.add(jb);
+	// System.out.println(db.read("0402213138").get(0).toString());
+	// System.out.println(db.read("0402213138").get(1).toString());
+	//
+	// }
+
 }
