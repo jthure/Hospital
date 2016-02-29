9fa34e34acf0dee9b2913b6c8c55acf1078a1fd3
Jonas Thuresson <dat13jth@student.lu.se>
2016-02-29 14:12:12 +0100

    lite commands

---
+++ src/model/CommandWrite.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import databas.DBHandler;
+import server.Journal;
+
+public class CommandWrite extends Command {
+
+	
+	public CommandWrite(String[] command) throws InvalidCommandException{
+		super(command);
+		LENGTH=4;
+		
+		
+		if(super.command.length!=LENGTH)
+			invalidArguments("Write");
+		
+	}
+
+
+	
+	@Override
+	Commands getCommand() {
+		return Commands.WRITE;
+	}
+
+	@Override
+	public String execute(DBHandler dbh)  throws IOException{
+		boolean append = Boolean.parseBoolean(command[3]);
+		boolean result = dbh.write(command[1], command[2], append);
+		if(result)
+			return "Journal successfully added";
+		return "Journal was not added";
+	}
+	
+}
